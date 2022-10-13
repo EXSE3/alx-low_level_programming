8881f1165337e106e044a611b0b6d9983555f4b6
EXSE3 <enamabinong@gmail.com>
2022-10-13 09:20:35 -0700

    advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,14 @@
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(2, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n",  
+	sizeof("and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19"));
 	return (1);
 }
