1889217e46b5599023d296193c665dc42f0b8cd6
EXSE3 <enamabinong@gmail.com>
2022-10-13 07:53:12 -0700

    advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	write( "and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n",58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
