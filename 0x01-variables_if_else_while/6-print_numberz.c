4418306bfe5cf5fe71b4add42b477549dec8b121
EXSE3 <enamabinong@gmail.com>
2022-10-14 16:37:22 +0100

    print the digit between 0and 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the digit between 0 and 9
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
+}
