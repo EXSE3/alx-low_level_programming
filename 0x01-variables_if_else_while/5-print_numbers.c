c35f43a2231e0ac24c37c33adf77b2966fd63243
EXSE3 <enamabinong@gmail.com>
2022-10-14 16:33:54 +0100

    print the digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the digit between 0 and 10
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+	printf("%i", i);
+}
+printf("\n");
+return (0);
+}
