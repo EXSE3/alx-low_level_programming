332658c9b53786054689d50c5a42e298cc0fbbe0
EXSE3 <enamabinong@gmail.com>
2022-10-14 16:31:01 +0100

    print the lowercase alphabet without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print the lowercase alphabet without e and q
+ * Description: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
