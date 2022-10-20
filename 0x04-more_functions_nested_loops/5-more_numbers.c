7c90254f14b95715064b680ad93457ddeca4c672
EXSE3 <enamabinong@gmail.com>
2022-10-20 17:11:15 +0100

    committing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,27 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * more_numbers - prints 10 times the numbers,
- * from 0 to 14, followed by a new line.
- * Return: Nothing.
- */
-
+  * more_numbers - Print 10 times the numbers since 0 up to 14
+  *
+  * Return: 10 times of the numbers since 0 up to 14
+  */
 void more_numbers(void)
 {
-	int i;
-        int j;
+	int x, y;
 
-	for (i = 0; i < 10; i++)
+	for (x = 0; x < 10; x++)
 	{
-		for (j = 0; j < 15; j++)
+		for (y = 0; y <= 14; y++)
 		{
-			_putchar('j');
-			j++;
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+
+			_putchar((y % 10) + '0');
 		}
+
 		_putchar('\n');
-		i++;
 	}
 }
+
